56b879cb5633acc83b790d2f757f8f6b37d88ae4
Herve-Henri2 <rvhouzard@gmail.com>
2021-11-17 14:37:52 +0100

    .

--- CodePython/CodePython/MainCode.py
+++ CodePython/CodePython/MainCode.py
@@ -31,5 +31,5 @@ for link in links:
 # PS: Lorsque vous voulez que votre code trouve un élément en particulier de la page, faire un clique droit -> inspecter pour trouver l'élément html correspondant 
 # et utiliser soup.find("nom de l'élément"). Pour plus d'infos sur html https://developer.mozilla.org/fr/docs/Web/HTML/Element
 # Si vous souhaitez enregistrer du code test sans risquer de détruire le code principal vous pouvez toujours créer une nouvelle branche séparée de la branche main et y téléverser votre code.
-# (Il sera toujours possible de fusionner les branches si le code s'avère plus complet.)
+# (/!\ Lorsque vous changez de branche ne les fusionnez pas à moins d'être sûr de la validité du code ajouté!)
                                                                                                 
