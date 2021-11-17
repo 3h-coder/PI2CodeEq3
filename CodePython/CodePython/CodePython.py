e1eadbc415438861b7b3dfc754f579e40537b17d
Herve-Henri2 <94490465+Herve-Henri2@users.noreply.github.com>
2021-11-17 09:51:42 +0100

    Update CodePython.py

--- CodePython/CodePython/CodePython.py
+++ CodePython/CodePython/CodePython.py
@@ -1,5 +1,5 @@
 #Le code se fera ici
 #faire tous les #imports nécessaires
 #regarder un tuto sur git hub / git hub desktop
-#installer les librairies nécessaires si non installées (exemple: pip install Bs4) 
+#installer les librairies nécessaires si non installées (exemple: pip install Bs4) test
 
