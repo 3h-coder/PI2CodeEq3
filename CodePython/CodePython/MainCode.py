71f25a42d64fd4100d5ec98599d37778b939f9fc
Herve-Henri2 <rvhouzard@gmail.com>
2021-11-25 21:10:39 +0100

    .

--- CodePython/CodePython/MainCode.py
+++ CodePython/CodePython/MainCode.py
@@ -13,36 +13,62 @@ from googlesearch import search
 import spacy 
 nlp = spacy.load('en_core_web_sm') #python -m spacy download en
 
+def introwebscraping():
+    query= "SolarWinds Cyberattaque" #La recherche que l'on effectue sur Google
+    links =[] #Liste qui contiendra tous les liens des sites webs que nous allons "scraper" à l'issue de la recherche.
 
-query= "SolarWinds Cyberattaque" #La recherche que l'on effectue sur Google
-links =[] #Liste qui contiendra tous les liens des sites webs que nous allons "scraper" à l'issue de la recherche.
-
-for j in search(query, num=3, stop=3, pause=0.5): #On se contente des 3 résultats jugés les plus pertinents par Google dans cet exemple, idéalement en prendre le plus possible.
+    for j in search(query, num=3, stop=3, pause=0.5): #On se contente des 3 résultats jugés les plus pertinents par Google dans cet exemple, idéalement en prendre le plus possible.
     #print(j)
-    #webbrowser.open(j)
-    links.append(j) #On sauvegarde les liens
+        webbrowser.open(j)
+        links.append(j) #On sauvegarde les liens
 
-for link in links:
-    print("-------------------------------------------------------------------------------------------------------------------------------------------------------")
-    page=requests.get(link)
-    soup=BeautifulSoup(page.text, "lxml")  #Lecture du code source de la page
-    print(link+"\n")
-    print(soup.find("title").text+"\n") #Titre de la page (à priori un article)
-    paragraphs=soup.find_all("p")
-    keywords=["cyberattaque"] #mots clé pour nous aider à extraire l'information voulue
-    keysentences=[] #phrases clé qu'il faudra analyser 
-    for paragraph in paragraphs: #Parcourir les paragraphes pour en extraire les informations relatives à une attaque ou faille de sécurité.
-        c=nlp(paragraph.text) #Conversion du texte en un objet spacy
-        sentences=list(c.sents) 
-        for sentence in sentences:
-            for keyword in keywords:
-                if keyword in str(sentence):
-                    print(sentence)
-                    keysentences.append(sentence)
+    for link in links:
+        print("-------------------------------------------------------------------------------------------------------------------------------------------------------")
+        page=requests.get(link)
+        soup=BeautifulSoup(page.text, "lxml")  #Lecture du code source de la page
+        print(link+"\n")
+        print(soup.find("title").text+"\n") #Titre de la page (à priori un article)
+        paragraphs=soup.find_all("p")
+        keywords=["cyberattaque"] #mots clé pour nous aider à extraire l'information voulue
+        keysentences=[] #phrases clé qu'il faudra analyser 
+        for paragraph in paragraphs: #Parcourir les paragraphes pour en extraire les informations relatives à une attaque ou faille de sécurité.
+            c=nlp(paragraph.text) #Conversion du texte en un objet spacy
+            sentences=list(c.sents) 
+            for sentence in sentences:
+                for keyword in keywords:
+                    if keyword in str(sentence):
+                        print(sentence)
+                        keysentences.append(sentence)
+                        
 
+def webscraping(entreprise): #/!\ La recherche est Case sensitive (les majuscules/minuscules sont différenciées)
+    #Faire un pattern tel que: 
+    #1 Dans un premier temps on regarde si l'entreprise apparait dans la page
+    #2 Si elle apparait dans un bloc qui mène à un lien, ouvrir le lien pour scraper la nouvelle page
+    sources=["https://www.darkreading.com/attacks-breaches"] #On part du principe que nos sources sont fiables ici
+
+    for source in sources:
+        mainpage=requests.get(source)
+        soup=BeautifulSoup(mainpage.text, "lxml")
+        anchors=soup.find_all('a')
+        for a in anchors:
+            if (entreprise in a.get('href')) or (entreprise in a.text):
+                newpage=requests.get(a.get('href'))
+                newsoup=BeautifulSoup(newpage.text, "lxml")
+                for paragraph in newsoup.find_all('p'):
+                    print(paragraph.text)
+                #webbrowser.open(link.get('href'))
+
+
+
+def main():
+    #print("Hello World!") #Remplacer cette ligne par la fonction à executer.
+    webscraping("Microsoft")
+
+
+main()
 
 # PS: Lorsque vous voulez que votre code trouve un élément en particulier de la page, faire un clique droit -> inspecter pour trouver l'élément html correspondant 
 # et utiliser soup.find("nom de l'élément"). Pour plus d'infos sur html https://developer.mozilla.org/fr/docs/Web/HTML/Element
 # Si vous souhaitez enregistrer du code test sans risquer de détruire le code principal vous pouvez toujours créer une nouvelle branche séparée de la branche main et y téléverser votre code.
 # (/!\ Lorsque vous changez de branche et revenez à la branche principale ne les fusionnez pas à moins d'être sûr de la validité du code ajouté!)
-                                                                                                
